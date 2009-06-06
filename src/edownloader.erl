5f4f0d9f330bd3b8189a848de79c1c7eba99fbb1
Jeremy Wall <jeremy@marzhillstudios.com>
2009-06-05 21:28:20 -0500

    download agents save to a temp file now so we dont have to store multi-gig downloads in memory

--- src/edownloader.erl
+++ src/edownloader.erl
@@ -1,16 +1,34 @@
 -module(edownloader).
 
+-include_lib("kernel/include/file.hrl").
+
 -export([start/0]).
--export([download_chunk/4, download_chunks/2]).
+-export([download_chunk/4, download_chunks/2, download_chunks_to/3]).
 -export([check_agent/1]).
 
--record(ed_agent_state, {stat, part, num, worker, range, percent=0}).
+-record(ed_agent_state, {stat, total=0, num=0, worker, range, percent=0, buffer, tries=0, url, tag, modified, marker=0}).
 
 start() -> ibrowse:start().
 
 get_head(Url) ->
     {ok, "200", Headers, _} = ibrowse:send_req(Url, [], head)
     , {ok, Headers}
+.
+
+download_chunks_to(Url, Count, File) ->
+    {ok, IoDevice} = file:open(File, [write])
+    , Buffers = download_chunks(Url, Count)
+    , [cat_file_to(IoDevice, B) || B <- Buffers]
+.
+
+cat_file_to(Dest, Source) ->
+    {ok, FileName} = file:pid2name(Source)
+    , io:format("file name: ~p", [FileName])
+    , file:close(Source)
+    , {ok, Data} =  file:read_file(FileName)
+    , io:format("file was read: ~p", [FileName])
+    , file:write(Dest, Data)
+    , file:delete(FileName)
 .
 
 download_chunks(Url, Count) ->
@@ -24,25 +42,25 @@ download_chunks(Url, Count) ->
         _ ->
             RangeList = edownload_util:get_range_list(Size, Count)
             , Agents = download_agent({Url, Tag, Modified}, RangeList, 1)
-            , accumulate_chunks(Agents)
+            , accumulate_buffers(Agents)
      end
 .
 
-accumulate_chunks([]) ->
+accumulate_buffers([]) ->
     [];
-accumulate_chunks(Agents) when is_list(Agents) ->
+accumulate_buffers(Agents) when is_list(Agents) ->
     [H | T] = Agents
-    , lists:flatten([accumulate_chunk(H) | accumulate_chunks(T)])
+    , [accumulate_buffer(H) | accumulate_buffers(T)]
 .
 
-accumulate_chunk(Agent) ->
+accumulate_buffer(Agent) ->
     case check_agent(Agent) of
         time_out ->
-            accumulate_chunk(Agent);
-        {ok, {_, Body}} ->
-            Body;
+            accumulate_buffer(Agent);
+        {ok, {_Num, Buffer}} ->
+            Buffer;
         {wait, _} ->
-            accumulate_chunk(Agent);
+            accumulate_buffer(Agent);
         Msg ->
             Msg
     end
@@ -54,7 +72,7 @@ download_agent(UrlInfo, RangeList, N) when is_list(RangeList) ->
     [H | T] = RangeList
     , [download_agent(UrlInfo, H, N) | download_agent(UrlInfo, T, N+1)];
 download_agent(UrlInfo, Range, Num) when is_tuple(Range) ->
-    Pid = spawn(fun() -> download_agent(#ed_agent_state{stat=wait, part=[], num=Num, range=Range}) end)
+    Pid = spawn(fun() -> download_agent(#ed_agent_state{stat=wait, total=0, num=Num, range=Range}) end)
     , Pid ! {start, {UrlInfo, Range}}
     , Pid
 .
@@ -62,29 +80,35 @@ download_agent(UrlInfo, Range, Num) when is_tuple(Range) ->
 
 download_agent(State) when is_record(State, ed_agent_state) ->
     Num = State#ed_agent_state.num
-    , Part = State#ed_agent_state.part
     , Stat = State#ed_agent_state.stat
+    , Total = State#ed_agent_state.total
+    , Marker = State#ed_agent_state.marker
     , Worker = State#ed_agent_state.worker
     , Percent = State#ed_agent_state.percent
+    , Buffer = State#ed_agent_state.buffer
     , receive
         {start, {{Url, Tag, Modified}, R}} ->
-            case download_chunk(Url, R, Tag, Modified) of
-                {error, Type} ->
-                    io:format("Error: ~p~n", [Type])
-                    ,  download_agent(State#ed_agent_state{stat=fail});
-                {ibrowse_req_id, Id} ->
-                    download_agent(State#ed_agent_state{worker=Id})
-            end;
+            FileName = integer_to_list(erlang:phash2(make_ref()))
+            , {ok, File} = file:open(FileName, [write, read])
+            , NewState = State#ed_agent_state{url=Url, tag=Tag, modified=Modified, range=R, buffer=File}
+            , start_chunk_download(NewState);
         {finished, Pid} ->
-            Pid ! {Stat, Num, {Part, Percent }};
+            Pid ! {Stat, Num, {Buffer, Percent }};
         {stop, Pid} ->
-            Pid ! {stopping, Num, Part}
+            Pid ! {stopping, Num, Buffer}
             , exit(normal);
         {ibrowse_async_headers, Worker, "206", _Headers} ->
             download_agent(State#ed_agent_state{stat=wait});
+        {ibrowse_async_response, Worker, {error, req_timedout}} ->
+            Tries = State#ed_agent_state.tries
+            , io:format("Warning: timedout. Tries: ~p~n", [Tries])
+            , start_chunk_download(State#ed_agent_state{tries=Tries + 1});
         {ibrowse_async_response, Worker, Body} ->
-            % TODO(jwall): log the percent complete for this agents work
-            NewState = calc_percent(State#ed_agent_state{stat=in_progress, part=Part ++ Body})
+            file:write(Buffer, Body)
+            , L = length(Body)
+            , NewState = calc_percent(State#ed_agent_state{stat=in_progress, total=Total+L,
+                marker=Marker+L})
+            , io:format("piece #~p is ~p% downloaded~n", [Num, NewState#ed_agent_state.percent])
             , download_agent(NewState);
         {ibrowse_async_response_end, Worker} ->
             download_agent(State#ed_agent_state{stat=ok, worker=Worker});
@@ -97,25 +121,25 @@ download_agent(State) when is_record(State, ed_agent_state) ->
     , download_agent(State)
 .
 
-calc_percent(State) ->
-    Total = length(State#ed_agent_state.part)
+start_chunk_download(State) ->
+    Url = State#ed_agent_state.url
     , {Start, End} = State#ed_agent_state.range
-    , P = trunc((Total / (End - Start)) * 100)
-    , State#ed_agent_state{percent=P}
-.
-
-check_agent(Pid) ->
-    Pid ! {finished, self()}
-    , receive
-        {ok, Num, {Body, _}} ->
-            {ok, {Num, Body}};
-        {fail, _, {Body, _}} ->
-            {fail, Body};
-        {Stat, Num, {_, _Percent}} when Stat == wait orelse Stat == in_progress ->
-            {wait, Num}
-    after
-        5*1000 ->
-            time_out
+    , R = case State#ed_agent_state.marker of
+        0 ->
+            {Start, End};
+        Start ->
+            {Start, End};
+        NewStart ->
+            {NewStart+1, End}
+    end
+    , Tag = State#ed_agent_state.tag
+    , Modified = State#ed_agent_state.modified
+    , case download_chunk(Url, R, Tag, Modified) of
+        {error, Type} ->
+            io:format("Error: ~p~n", [Type])
+            ,  download_agent(State#ed_agent_state{stat=fail});
+        {ibrowse_req_id, Id} ->
+            download_agent(State#ed_agent_state{worker=Id, marker=Start})
     end
 .
 
@@ -127,5 +151,27 @@ download_chunk(Url, {Start, End}, Tag, Modified) ->
     , io:format("Headers: ~p~n", [Headers])
     %% TODO(jwall): configurable authentication
     , ibrowse:send_req(Url, Headers, get, Body, Options)
+.
+
+check_agent(Pid) ->
+    Pid ! {finished, self()}
+    , receive
+        {ok, Num, {Buffer, _}} ->
+            {ok, {Num, Buffer}};
+        {fail, _, {Buffer, _}} ->
+            {fail, Buffer};
+        {Stat, Num, {_, _Percent}} when Stat == wait orelse Stat == in_progress ->
+            {wait, Num}
+    after
+        5*1000 ->
+            time_out
+    end
+.
+
+calc_percent(State) ->
+    Total = State#ed_agent_state.total
+    , {Start, End} = State#ed_agent_state.range
+    , P = trunc((Total / (End - Start)) * 100)
+    , State#ed_agent_state{percent=P}
 .
 
